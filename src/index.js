b10ba9eccf702b0d1c05bc1f2512adc9126560e8
Oleh <olegkaraaa@gmail.com>
2021-10-07 18:22:16 +0300

    deploy test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+const API_KEY = `c4da2d26df740b651b6bb4b7cba32696`;
+fetch(
+  `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=uk-UK&page=1&include_adult=false&query=dog`,
+)
+  .then(d => d.json())
+  .then(d => console.log(d));
