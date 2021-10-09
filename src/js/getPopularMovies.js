67f3bdd7a7aa96a1971538f03e68a225f7dd49ba
Oleh <olegkaraaa@gmail.com>
2021-10-09 22:02:48 +0300

    Доработки
    1. Добавил преобразование списка жанров если их больше трех
    2. Стили для обрезания длинных жанров
    3. Фикс паддингов на разных экранах

--- src/js/getPopularMovies.js
+++ src/js/getPopularMovies.js
@@ -4,32 +4,29 @@ import genres_decoder from './genres_decoder';
 import { renderMovieMarkup } from './renderMovieMarkup.js';
 import template from '../templates/film-card-li.hbs';
 
-
-
 const movieApi = new FetchApi();
 
- function pad(value)  {
-    return String(value).padEnd(3, '.0');
-  }
+function pad(value) {
+  return String(value).padEnd(3, '.0');
+}
 
 movieApi.getPopularMovies().then(d => {
-
   const modifiedData = d.results.map(m => {
     m.genre_ids = genres_decoder(m.genre_ids);
 
     if (m.genre_ids.length > 3) {
-      m.genre_ids = m.genre_ids.slice(0, 3);
-        
+      m.genre_ids = m.genre_ids.slice(0, 2);
+      m.genre_ids.push('Other');
     }
-    m.genre_ids = m.genre_ids.join(", ");
-    if (m.release_date !== undefined) { m.release_date = m.release_date.slice(0, 4) }
-    else { m.first_air_date = m.first_air_date.slice(0, 4) }
-    m.vote_average = pad(m.vote_average)
+    m.genre_ids = m.genre_ids.join(', ');
+    if (m.release_date !== undefined) {
+      m.release_date = m.release_date.slice(0, 4);
+    } else {
+      m.first_air_date = m.first_air_date.slice(0, 4);
+    }
+    m.vote_average = pad(m.vote_average);
     return m;
   });
-  
+
   renderMovieMarkup(template, modifiedData);
 });
-  
-  
-
