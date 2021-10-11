baf3f8df630ccb39e86559b4682e4663ce2d7692
Oleh <olegkaraaa@gmail.com>
2021-10-11 23:45:33 +0300

    Доработка
    1. Поправил запрос по ID фильма
    2. Поправил импорт шаблона
    3. Поправил сам шаблон
    4. Поправил стили

--- src/js/renderMovieMarkup.js
+++ src/js/renderMovieMarkup.js
@@ -1,11 +1,8 @@
-
 const refs = {
   movieListRef: document.querySelector('.movie-list'),
-}
-
+};
 
 export const renderMovieMarkup = (template, arr) => {
-  const MovieMarkup = template(arr)
-  return refs.movieListRef.innerHTML = MovieMarkup;
-}
-
+  const MovieMarkup = template(arr);
+  return (refs.movieListRef.innerHTML = MovieMarkup);
+};
--- src/js/renderMovieModal.js
+++ src/js/renderMovieModal.js
@@ -1,83 +1,67 @@
-
- // import movieTpl from '../templates/film-card_modal.hbs';
-
-
+import movieTpl from '../templates/film-card_modal.hbs';
+import movie_Api from './movie_Api';
+const api = new movie_Api();
 
 const refs = {
-    filmCard: document.querySelector('.movie-list'), 
-    modalBackdrop: document.querySelector('.backdrop-js'),
-    modalCloseBtn: document.querySelector('[data-modal-close]'),
-    movieMarckup: document.querySelector('.template-container'),
-    
+  filmCard: document.querySelector('.movie-list'),
+  modalBackdrop: document.querySelector('.backdrop-js'),
+  modalCloseBtn: document.querySelector('[data-modal-close]'),
+  movieMarkup: document.querySelector('.template-container'),
+};
+
+refs.filmCard.addEventListener('click', onFilmClick);
+
+function addEventListeners() {
+  refs.modalCloseBtn.addEventListener('click', closeModal);
+
+  window.addEventListener('keydown', closeModalByKeydown);
+
+  refs.modalBackdrop.addEventListener('click', closeModalByBackdropClick);
+}
+
+function removeEventListeners() {
+  refs.modalCloseBtn.removeEventListener('click', closeModal);
+
+  window.removeEventListener('keydown', closeModalByKeydown);
+
+  refs.modalBackdrop.removeEventListener('click', closeModalByBackdropClick);
+}
+
+function openModal() {
+  refs.modalBackdrop.classList.remove('is-hidden');
+  document.body.style.overflow = 'hidden';
+  addEventListeners();
+}
+
+function closeModal() {
+  refs.modalBackdrop.classList.add('is-hidden');
+  document.body.style.overflow = '';
+  removeEventListeners();
+}
+
+function closeModalByKeydown(e) {
+  if (e.code !== 'Escape') {
+    return;
   }
+  closeModal();
+}
 
-
-  refs.filmCard.addEventListener('click', onFilmClick)
-
-
-  function addEventListeners () {
-    refs.modalCloseBtn.addEventListener('click', closeModal);
-
-    window.addEventListener('keydown', closeModalByKeydown);
-  
-    refs.modalBackdrop.addEventListener('click', closeModalByBackdropClick);
+function closeModalByBackdropClick(e) {
+  if (e.target.classList.contains('backdrop-js')) {
+    closeModal();
   }
+}
 
-  
-  function removeEventListeners () {
-    refs.modalCloseBtn.removeEventListener('click', closeModal);
+function onFilmClick(e) {
+  const id = e.target.closest('li').dataset.movieId;
+  openModal();
+  api.getMovieInfo(id).then(d => {
+    renderMovieMarckup(d);
+    console.log(d);
+  });
+}
 
-    window.removeEventListener('keydown', closeModalByKeydown);
-  
-    refs.modalBackdrop.removeEventListener('click', closeModalByBackdropClick);
-  }
-
-
-  function openModal () {
-    refs.modalBackdrop.classList.remove('is-hidden')
-    document.body.style.overflow = 'hidden' 
-    addEventListeners ()
-   
-  }
-
-  function closeModal () {
-    refs.modalBackdrop.classList.add('is-hidden')
-    document.body.style.overflow = ''
-    removeEventListeners ()
-  }
-
-  function closeModalByKeydown (e) {
-    if(e.code !=='Escape') {
-      return
-    }
-    closeModal()
-  }
-
-  function closeModalByBackdropClick(e) {
-    if(e.target.classList.contains('backdrop-js')) {
-      closeModal()    
-    }
-  }
-    
-  
-
-  function onFilmClick (e) {
-    
-
-    if(e.target.tagName = 'li') {
-      openModal()
-      const movie = e.target.closest('li').dataset.movieId;
-      console.log(movie)
-      
-    } 
-     return 
-  }
-
-
-//   function renderMovieMarckup (movie) {
-    
-//     const markup = movieTpl(movie);
-//     movieMarkup.insertAdjacentHTML('afterbegin', markup)
-// }
-
-
+function renderMovieMarckup(movie) {
+  const markup = movieTpl(movie);
+  refs.movieMarkup.innerHTML = markup;
+}
