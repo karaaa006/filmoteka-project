bd65776f86325c128cc997b2d02c5415029d2bb4
nelli-belovol <nelli199020@gmail.com>
2021-10-12 18:24:31 +0300

    инф о фильмах в local storage и отрисовка в my Library

--- src/js/control-buttons.js
+++ src/js/control-buttons.js
@@ -1,30 +1,39 @@
-const controlButtons = document.querySelector('.dinamic-content')
+import LSService from './storage';
+const lsService = new LSService();
 
+import { renderMovieMarkup } from './renderMovieMarkup';
+import template from '../templates/film-card-li.hbs';
+const controlButtons = document.querySelector('.dinamic-content');
 
 export default function changeButtonsColor() {
-    showWatchedFilms()
-    controlButtons.addEventListener('click', (event) => {
-        const watchedButton = document.querySelector('#watched')
-        const queueButton = document.querySelector('#queue')
-        if (event.target.classList.contains('active')) {
-            return
-        } else if (event.target === watchedButton) {
-            queueButton.classList.remove('active')
-            watchedButton.classList.add('active')
-            showWatchedFilms()
-        } else if (event.target === queueButton) {
-            watchedButton.classList.remove('active')
-            queueButton.classList.add('active')
-            showQueue()
-        }
-    })
+  showWatchedFilms();
+  controlButtons.addEventListener('click', event => {
+    const watchedButton = document.querySelector('#watched');
+    const queueButton = document.querySelector('#queue');
+    if (event.target.classList.contains('active')) {
+      return;
+    } else if (event.target === watchedButton) {
+      queueButton.classList.remove('active');
+      watchedButton.classList.add('active');
+      showWatchedFilms();
+    } else if (event.target === queueButton) {
+      watchedButton.classList.remove('active');
+      queueButton.classList.add('active');
+      showQueue();
+    }
+  });
 }
 
 // Вместо этих двух функций должен быть вызов функций по отрисовке библиотеки пользователя
 
 function showWatchedFilms() {
-    console.log('показать просмотренные фильмы')
+  console.log('показать просмотренные фильмы');
+  let array = lsService.getFromWatchedLS();
+  console.log(array);
+  renderMovieMarkup(template, array);
 }
-function showQueue(){
-    console.log('показать очередь воспроизведения')
+function showQueue() {
+  console.log('показать очередь воспроизведения');
+  let array = lsService.getQueueLS();
+  renderMovieMarkup(template, array);
 }
