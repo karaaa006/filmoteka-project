9acc963f8936abe2c33ab44dce94c6d3dc1dad3e
Oleh <olegkaraaa@gmail.com>
2021-10-16 10:46:29 +0300

    Инициализация Firebase

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import './sass/main.scss';
+import './js/firebase';
 import './js/storage';
 import './js/renderMovieModal';
 import './js/nav-buttons';
 import './js/getPopularMovies.js';
 import './js/slider';
-
-import paginationButtons from './js/paginationButtons.js';
-
-// import requestApi from './js/movie_Api';
-// import genres_decoder from './js/genres_decoder';
-// import changeButtonsColor from './js/control-buttons';
-
-// console.log('Преобразование ID жанров в их название: ', genres_decoder([10759, 9648, 18]));
-
-// const request = new requestApi();
-// request.searchMovies('INJUSTICE').then(d => console.log('Поиск фильмов по запросу: ', d));
-// request.getPopularMovies().then(d => console.log('Популярные фильмы: ', d));
-// request.getMovieInfo('122').then(d => console.log('Информация о фильме по ID: ', d));
-// request.getUpcomingMovies().then(d => console.log('Скоро в кино: ', d));
-
-
-
+import './js/paginationButtons.js';
---
+++ src/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-6Ag3yMGXaVI-EsqE4UcD4orRnsklp6k',
+  authDomain: 'filmoteka-a6dd0.firebaseapp.com',
+  projectId: 'filmoteka-a6dd0',
+  storageBucket: 'filmoteka-a6dd0.appspot.com',
+  messagingSenderId: '152374939851',
+  appId: '1:152374939851:web:d5860e5907ada683688b4e',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
