3ee8204ae720cc464f559eadb385f5ddcd8a2ce7
Oleh <olegkaraaa@gmail.com>
2021-10-17 14:00:20 +0300

    Скопировал изменения Егора, немного поправил дизайн, удалил Ваню

---
+++ src/js/students.js
@@ -0,0 +1,27 @@
+import students from './studentsData.json';
+import studentsTemplate from '../templates/students-modal.hbs';
+import * as basicLightbox from 'basiclightbox';
+
+const refs = {
+  studentGoit: document.querySelector('.footer__link'),
+  backdrop: document.querySelector('[data-modal]'),
+  modal: document.querySelector('.modal-students'),
+};
+
+const markupStudentsModal = studentsTemplate(students);
+const instance = basicLightbox.create(markupStudentsModal);
+
+refs.studentGoit.addEventListener('click', onOpenModal);
+
+function onOpenModal(e) {
+  instance.show();
+
+  window.addEventListener('keydown', onCloseModal);
+
+  function onCloseModal(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onCloseModal);
+    }
+  }
+}
