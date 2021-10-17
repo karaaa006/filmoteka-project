93d65feb753113b88df147fc2006d7ef45af473f
Oleh <olegkaraaa@gmail.com>
2021-10-17 20:11:38 +0300

    Исправил стили кнопок слайдера

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import './js/slider';
 import './sass/main.scss';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import './js/storage';
 import './js/renderMovieModal';
 import './js/nav-buttons';
 import './js/getPopularMovies.js';
-import './js/slider';
 import './js/students';
 
 import './js/paginationButtons';
